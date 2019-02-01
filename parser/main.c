3b5ab37c6d72b67f2537ea0e142473e18cced384
dangkexi <dangkexi@gmail.com>
2019-01-31 21:13:27 -0600

    main function handle 2 or 3 arguments

--- parser/main.c
+++ parser/main.c
@@ -3,6 +3,8 @@ int yylineno;
 int succFlag;
 int errorCount = 0;
 
+FILE *yyin;
+
 // must have yyerror, yywrap
 int yyerror (const char *s) {	
 	errorCount += 1;
@@ -12,9 +14,51 @@ int yyerror (const char *s) {
 int yywrap (void) {return 1;}  // means end
 
 int main (int argc, char* argv[]) {
-	if (argc == 2) {
-
+	int printHelp = 0;
+	// ./demo -h test.demo
+	if (argc == 3) {
+		char *tmp = "-h";
+		if (strcmp(argv[1], tmp) == 0 ){
+			printHelp = 1;
+			yyin = fopen(argv[2], "r");
+			if (!yyin){
+				printf("error, unable to open file %s", argv[2]);
+				return 0;
+			}
+		}else if(argv[1][0] == '-'){
+			printf("no option '-%c', please use -h to check.\n", argv[1][1]);
+			return 0;
+		}else {
+			printf("wrong parameter, please use -h to check.\n");
+			return 0;
+		}
 	}
+	// ./demo -h  or ./demo a.demo
+	else if(argc == 2) {
+		char *tmp = "-h";
+		if (strcmp(argv[1], tmp) == 0 ){
+			printHelp = 1;
+		}else if(argv[1][0] == '-'){
+			printf("no option '-%c', please use -h to check.\n", argv[1][1]);
+			return 0;
+		}else {
+			yyin = fopen(argv[1], "r");
+			if(!yyin) {
+				printf("error, unable to open file %s", argv[1]);
+				return 0;
+			}
+		}
+	}
+
+	// handle all printHelp
+	if (printHelp == 1){
+        printf("======== Command help start ========\n");
+        printf("./demo -h: check command line syntax\n");
+        printf("./demo filename: compile target file\n");
+        printf("./demo: compile from stdin, ^D to EOF\n");
+		printf("======== Command help end ========\n");
+		return 0;
+    }
 
 	succFlag = yyparse();
 
